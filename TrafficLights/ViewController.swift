4dc515130837747e7d41618718d0291eeb9cfba7
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-01-31 23:46:47 +0300

    Connecting views and button form Main to ViewControl.

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var startBotton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        startBotton.layer.cornerRadius = 12
     }
 
+    @IBAction func startButtonPressed() {
+        
+        startBotton.setTitle("NEXT", for: .normal)
 
-}
+        if redLight.alpha != 1 {
+            redLight.alpha = 1
+            yellowLight.alpha = 0.3
+        } else if yellowLight.alpha != 1 {
+            yellowLight.alpha = 1
+            redLight.alpha = 0.3
+        } else if greenLight.alpha != 1 {
+            greenLight.alpha = 1
+            yellowLight.alpha = 0.3
+            redLight.alpha = 0.3
+        }
+        }
 
+    }
